3f9bb5078e94781e74ff8392abe49bfc1a4e76c9
emines0 <emin.sefa1@gmail.com>
2022-10-07 16:49:06 +0200

    addin first function onButtonSubmit, onButtonChange

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -6,16 +7,36 @@ import Rank from "./components/Rank/Rank";
 import ParticlesBg from 'particles-bg';
 
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg color="#ffffff" num={250} type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm  />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg color="#ffffff" num={250} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
